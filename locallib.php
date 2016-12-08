09ba53af98dd3aea290b588cc6b09655b7da3fa9
jfederico <jesus@123it.ca>
2016-12-08 16:45:21 -0500

    Refactoring to save file and added localib

---
+++ locallib.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Atto recordrtc library functions
+ *
+ * @package    tinymce_recordrtc
+ * @author     Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ * @copyright  2016 Blindside Networks Inc.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once(dirname(__FILE__).'/lib.php');
+
--- save.php
+++ save.php
@@ -1,18 +1,39 @@
 <?php
-// Muaz Khan     - www.MuazKhan.com
-// MIT License   - https://www.webrtc-experiment.com/licence/
-// Documentation - https://github.com/muaz-khan/WebRTC-Experiment/tree/master/RecordRTC
-foreach(array('video', 'audio') as $type) {
-    if (isset($_FILES["${type}-blob"])) {
-        echo 'uploads/';
 
+/**
+ * @package    tinymce_recordrtc
+ * @author     Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ * @copyright  2016 Blindside Networks Inc.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))).'/config.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+foreach(array('video', 'audio') as $type) {
+    if ( !isset($_FILES["${type}-blob"]) ) {
+        error_log("Blob not included");
+        header("HTTP/1.0 400 Bad Request");
+        return;
+    } else if ( !isset($_POST["${type}-filename"]) ) {
+        error_log("Filename not included");
+        header("HTTP/1.0 400 Bad Request");
+        return;
+    } else {
         $fileName = $_POST["${type}-filename"];
         $uploadDirectory = 'uploads/'.$fileName;
 
         if (!move_uploaded_file($_FILES["${type}-blob"]["tmp_name"], $uploadDirectory)) {
-            echo(" problem moving uploaded file");
+            error_log("Problem moving uploaded file");
+            header("HTTP/1.0 500 Internal Server Error");
+            return;
+        } else {
+            // OK response
+            echo($uploadDirectory);
         }
-
-        echo($fileName);
+    } else {
+        error_log("Blob not included");
+        header("HTTP/1.0 400 Bad Request");
+        return;
     }
 }
