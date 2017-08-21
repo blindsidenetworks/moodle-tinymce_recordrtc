ba2ae14b306884076aabb7ec3b59e376b324a76f
jfederico <jesus@123it.ca>
2017-08-21 19:56:16 +0000

    Bumped up version tags to b4

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017070102;
+$plugin->version   = 2017070103;
 $plugin->requires  = 2015051100;
 $plugin->component = 'tinymce_recordrtc';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '1.0-b3';
+$plugin->release = '1.0-b4';
