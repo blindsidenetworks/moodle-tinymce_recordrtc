52b0cb6f3146563192d6cfc996524424eea90974
Jacob Prud'homme <jacob.prudhomme@blindsidenetworks.com>
2017-08-24 13:03:40 -0400

    Put stop_recording in commonmodule

--- tinymce/js/commonmodule.js
+++ tinymce/js/commonmodule.js
@@ -159,6 +159,17 @@ M.tinymce_recordrtc.start_recording = function(type, stream) {
     startStopBtn.set('disabled', false);
 };
 
+M.tinymce_recordrtc.stop_recording = function(stream) {
+    // Stop recording microphone stream.
+    mediaRecorder.stop();
+
+    // Stop each individual MediaTrack.
+    var tracks = stream.getTracks();
+    for (var i = 0; i < tracks.length; i++) {
+        tracks[i].stop();
+    }
+};
+
 // Upload recorded audio/video to server.
 M.tinymce_recordrtc.upload_to_server = function(type, callback) {
     var xhr = new window.XMLHttpRequest();
