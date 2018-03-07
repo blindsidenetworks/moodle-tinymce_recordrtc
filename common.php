7866e877eeb63d92ffd3741c375a9dbe3123669a
Mirau89 <marouene.agrebi@riadvice.tn>
2018-03-07 17:00:10 +0100

    Try N°9

--- common.php
+++ common.php
@@ -1,10 +1,29 @@
 <?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
 /**
  * Created by PhpStorm.
  * User: riadvice
  * Date: 02/03/18
  * Time: 02:47 م
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 $PAGE->set_cacheable(false);
 $title = '';
